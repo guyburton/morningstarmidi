5c178119ebd7cde9a1083c87d97b119b1454b365
Guy Burton <guy.burton@isam.com>
2020-04-10 07:29:58 +0100

    Add debug utility to midi.py

--- morningstar/midi.py
+++ morningstar/midi.py
@@ -6,12 +6,13 @@ try:
 
     can_send = True
     ports = mido.get_output_names()
-    if device_name not in ports:
-        print("Available ports: " + ", ".join(ports))
-    for port in ports:
-        if port.startswith(device_name):
-            device_name = port
-            break
+    print("Available ports: " + ", ".join(ports) + " (default is '" + device_name + "')")
+    if len(ports) == 1:
+        device_name = ports[0]
+    else:
+        for port in ports:
+            if port.startswith(device_name):
+                device_name = port
 
 except ImportError:
     print("Could not load module mido- run 'pip install mido' if you wish to send midi commands directly")
@@ -22,3 +23,7 @@ def send(data_bytes):
         port = mido.open_output(device_name)
         # do we need to send a command first to start upload?
         port.send(mido.Message('sysex', data=data_bytes[1:-1]))
+
+
+if __name__ == "__main__":
+    pass  # don't need to do anything since we print during module import
